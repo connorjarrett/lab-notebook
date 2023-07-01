eaf26c72276f33d6beea69733a02793937dc52ca
Connor Jarrett <connor.jarrett07@gmail.com>
2023-07-01 22:53:58 +0100

    Youtube uses www.nocookie instead of nocookie to prevent https error

--- tools/build.js
+++ tools/build.js
@@ -138,7 +138,7 @@ const e = new Promise((resolveOuter) => {
 
                             if (content.includes("<p>[video]</p>")) {
                                 if (this.attribute("video")) {
-                                    content = content.replaceAll("<p>[video]</p>", `<iframe class="youtube" src="https://youtube-nocookie.com/embed/${this.attribute("video")}"></iframe>`)
+                                    content = content.replaceAll("<p>[video]</p>", `<iframe class="youtube" src="https://www.youtube-nocookie.com/embed/${this.attribute("video")}"></iframe>`)
                                 }
                             }
 
